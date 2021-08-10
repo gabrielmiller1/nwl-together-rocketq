0d5488a25d8a5e87892b92fbc278d6a9f306150e
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-08-09 21:31:28 -0300

    Creating modal interations and preparing to controllers

--- src/route.js
+++ src/route.js
@@ -6,5 +6,7 @@ route.get('/', (req, res) => res.render('home.ejs'));
 route.get('/create-pass', (req, res) => res.render('create-pass.ejs'));
 route.get('/room', (req, res) => res.render('room.ejs'));
 
+route.post('/:room/:question/:action', (req, res) => res.render(exemplo, {req}))
+
 
 module.exports = route;
