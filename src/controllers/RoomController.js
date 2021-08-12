e7da37da07c5c40ed914266de30cb5a21c649bb0
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-08-12 15:16:39 -0300

    Finishing room page and fetching data from the database

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -4,23 +4,41 @@ module.exports = {
     async create(req, res) {
         const db = await Database();
         const pass = req.body.password;
-        let roomId = "";
+        let roomId ="";
+        let isRoom = true;
 
+        while (isRoom) {
 
-        for(var i = 0; i < 6; i++){
-            roomId += Math.floor(Math.random() * 5).toString();
+            for (var i = 0; i < 6; i++) {
+                roomId += Math.floor(Math.random() * 5).toString();
+            }
+
+            const roomsExistId = await db.all(`SELECT id FROM rooms`);
+
+            isRoom = roomsExistId.some(roomsExistId => { roomsExistId == roomId });
+
+            if (!isRoom) {
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`);
+            }
         }
-
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`);
 
         await db.close();
 
         res.redirect(`/room/${roomId}`);
+    },
+
+    async open(req, res) {
+        const db = await Database();
+        const roomId = req.params.room;
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and checked = 0`);
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE checked = ${roomId} and checked = 1`);
+        
+        res.render('room.ejs', { roomId: roomId, questions: questions, questionsRead: questionsRead });
     }
 }
--- src/route.js
+++ src/route.js
@@ -8,9 +8,11 @@ const route = express.Router();
 
 route.get('/', (req, res) => res.render('index.ejs', {page: 'enter-room'}));
 route.get('/create-pass', (req, res) => res.render('index.ejs', {page: 'create-pass'}));
-route.get('/room/:room', (req, res) => res.render('room.ejs'));
 
-route.post('/question/:room/:question/:action', QuetionController.index);
 route.post('/create-room', RoomController.create);
+route.get('/room/:room', RoomController.open);
+
+route.post('/question/create/:room', QuetionController.create);
+route.post('/question/:room/:question/:action', QuetionController.index);
 
 module.exports = route;
