15a88565eb7ca5be4d39fef69191bb46c53c7684
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-08-12 22:46:40 -0300

    Implementing clickToCopy on room page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,10 +6,10 @@ const modalTitle = document.querySelector('.modal h2')
 const modalDescription = document.querySelector('.modal p')
 const modalButton = document.querySelector('.modal button')
 
-const checkButtons = document.querySelectorAll(".actions a.check") 
+const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button => {
-    
+
     button.addEventListener("click", handleClick)
 })
 
@@ -20,7 +20,7 @@ deleteButton.forEach(button => {
     button.addEventListener("click", (event) => handleClick(event, false))
 })
 
-function handleClick(event, check = true){
+function handleClick(event, check = true) {
     event.preventDefault()
     const text = check ? "Marcar como lida" : "Excluir"
     const slug = check ? "check" : "delete"
@@ -30,11 +30,33 @@ function handleClick(event, check = true){
     const form = document.querySelector(".modal form")
     form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
 
-    modalTitle.innerHTML= `${text} esta pergunta`
-    modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
-    modalButton.innerHTML= `Sim, ${text.toLowerCase()}`
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
     check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
-    
+
     modal.open()
+};
+
+
+const buttonCopy = document.getElementById('room-id')
+
+buttonCopy.addEventListener('click', copyId);
+
+function copyId(){
+
+    let inputElement = document.createElement('input');
+    inputElement.type = 'text';
+
+    let copiedContent = buttonCopy.getAttribute("data-id");
+
+    inputElement.value = copiedContent;
+    document.body.appendChild(inputElement);
+    inputElement.select();
+    document.execCommand('copy');
+    document.body.removeChild(inputElement);
+
+    alert(`O texto copiado é: ${copiedContent}`);
 }
+
 
